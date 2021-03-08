44bf5cdeb9ac447c56ef367479720fcdc1e73123
rizka_amelia_yasmin <rizkayasmin@student.telkomuniversity.ac.id>
2021-03-08 14:57:06 +0700

    Menambahkan gambar pada list

--- app/src/main/java/org/d3if4004/galerihewan/MainActivity.kt
+++ app/src/main/java/org/d3if4004/galerihewan/MainActivity.kt
@@ -4,7 +4,6 @@ import Hewan
 import MainAdapter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.RecyclerView
 import org.d3if4004.galerihewan.databinding.ActivityMainBinding
@@ -27,16 +26,16 @@ class MainActivity : AppCompatActivity() {
 
     private fun getData(): List<Hewan> {
         return listOf(
-                Hewan("Angsa", "Cygnus olor"),
-                Hewan("Ayam", "Gallus gallus"),
-                Hewan("Bebek", "Cairina moschata"),
-                Hewan("Domba", "Ovis ammon"),
-                Hewan("Kalkun", "Meleagris gallopavo"),
-                Hewan("Kambing", "Capricornis sumatrensis"),
-                Hewan("Keinci", "Oryctolagus cuniculus"),
-                Hewan("Kerbau", "Bubalus bubalis"),
-                Hewan("Kuda", "Equus caballus"),
-                Hewan("Sapi", "Bos Taurus")
+            Hewan("Angsa", "Cygnus olor", R.drawable.angsa),
+            Hewan("Ayam", "Gallus gallus", R.drawable.ayam),
+            Hewan("Bebek", "Cairina moschata", R.drawable.bebek),
+            Hewan("Domba", "Ovis ammon", R.drawable.domba),
+            Hewan("Kalkun", "Meleagris gallopavo", R.drawable.kalkun),
+            Hewan("Kambing", "Capricornis sumatrensis", R.drawable.kambing),
+            Hewan("Kelinci", "Oryctolagus cuniculus", R.drawable.kelinci),
+            Hewan("Kerbau", "Bubalus bubalis", R.drawable.kerbau),
+            Hewan("Kuda", "Equus caballus", R.drawable.kuda),
+            Hewan("Sapi", "Bos taurus", R.drawable.sapi)
         )
     }
 }
