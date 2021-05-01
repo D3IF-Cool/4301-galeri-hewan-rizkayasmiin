34d5065d6c991f42a4007b3d5cd49819d6c2bad4
rizka_amelia_yasmin <rizkayasmin@student.telkomuniversity.ac.id>
2021-05-01 15:00:38 +0700

    Menambahkan loading indicator

--- app/src/main/java/org/d3if4004/galerihewan/ui/main/MainViewModel.kt
+++ app/src/main/java/org/d3if4004/galerihewan/ui/main/MainViewModel.kt
@@ -7,24 +7,30 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.launch
 import org.d3if4004.galerihewan.model.Hewan
+import org.d3if4004.galerihewan.network.ApiStatus
 import org.d3if4004.galerihewan.network.HewanApi
 import java.lang.Exception
 
 class MainViewModel : ViewModel() {
     private val data = MutableLiveData<List<Hewan>>()
+    private val status = MutableLiveData<ApiStatus>()
     init {
         retrieveData()
     }
 
     private fun retrieveData() {
         viewModelScope.launch {
+            status.value = ApiStatus.LOADING
             try {
                 data.value = HewanApi.service.getHewan()
+                status.value = ApiStatus.SUCCESS
             } catch (e: Exception) {
                 Log.d("MainViewModel", "Failure: ${e.message}")
+                status.value = ApiStatus.FAILED
             }
         }
     }
 
     fun getData(): LiveData<List<Hewan>> = data
+    fun getStatus(): LiveData<ApiStatus> = status
 }
